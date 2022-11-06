69ee0391d63937903dd1c38c131e6f9e9d6616a6
Kyrylo1122 <93613807+Kyrylo1122@users.noreply.github.com>
2022-11-06 17:48:02 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Layout from "./Components/Layout/Layout";
 import "./sass/main.scss";
 import { ToastContainer } from "react-toastify";
 import Studio from "./Components/Studio/Studio";
-import Portfolio from "./Components/Portfolio/Portfolio";
 import { HeaderNavNames } from "./Components/AppBar/Navigation/HeaderNavNames";
 
 function App() {
@@ -16,7 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Studio />} />
-          <Route path={`${HeaderNavNames.PORTFOLIO}`} element={<Portfolio />} />
+          <Route
+            path={`${HeaderNavNames.PORTFOLIO}`}
+            element={<h1>Portfolio</h1>}
+          />
         </Route>
       </Routes>
     </div>
